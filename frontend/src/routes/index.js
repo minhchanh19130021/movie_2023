aca5381ef68048098b7a9f70b9e9681e937e5037
nguyenhoaibao01 <76415346+nguyenhoaibao01@users.noreply.github.com>
2023-06-25 10:11:24 +0700

    sign up page

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,10 +3,12 @@ import { Home } from '~/pages/Home';
 import { SignIn } from '~/pages/SignIn';
 import { Detail } from '~/pages/Detail';
 import { Search } from '~/pages/Search';
+import { SignUp } from '~/pages/SignUp';
 
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.signIn, component: SignIn },
+    { path: config.routes.signUp, component: SignUp },
     { path: config.routes.detail, component: Detail },
     { path: config.routes.search, component: Search },
 
