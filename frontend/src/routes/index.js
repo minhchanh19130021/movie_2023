5e0dee1c6830e30fb8082a413f71a6069b4cc61e
Dang Thi Thanh Hue <91484695+thanhhue723@users.noreply.github.com>
2023-06-25 20:04:03 +0700

    bổ sung  môi trường

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,15 +1,14 @@
 import config from '~/config';
 import { Home } from '~/pages/Home';
-import { SignIn } from '~/pages/SignIn';
 import { Detail } from '~/pages/Detail';
 import { Search } from '~/pages/Search';
+import { Login } from '~/pages/Login';
 
 const publicRoutes = [
     { path: config.routes.home, component: Home },
-    { path: config.routes.signIn, component: SignIn },
     { path: config.routes.detail, component: Detail },
     { path: config.routes.search, component: Search },
-
+    { path: config.routes.login, component: Login },
 ];
 const privateRoutes = [];
 export { publicRoutes, privateRoutes };
